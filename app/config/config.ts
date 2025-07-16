6da12f1c29f7cd5516f1eeb40e302ac13af75154
Ahmed Raza <101836398+devdazzlee@users.noreply.github.com>
2025-07-16 15:49:19 +0500

    Update Varaibles

--- app/config/config.ts
+++ app/config/config.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVAvNCkDSzo9Mi6QeqoF9xRZ-hfSMPGWY",
-  authDomain: "test-cbccc.firebaseapp.com",
-  projectId: "test-cbccc",
-  storageBucket: "test-cbccc.firebasestorage.app",
-  messagingSenderId: "739522744963",
-  appId: "1:739522744963:web:cc80dbe14757bdfd8941b6",
-  measurementId: "G-2YQ75PHMVB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
